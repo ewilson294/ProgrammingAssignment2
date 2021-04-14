b1aae1661b02d4fd17bb9dc7556c7f035ee37026
ewilson294 <ecwil2@yahoo.com>
2021-04-13 20:20:21 -0500

    First commit. Initial version of  "makeCacheMatrix"

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a "matrix" with functions to set the value of a matrix,
+## get the value of a matrix, set the value of the inverse of a matrix,
+## and get the value of the inverse of a matrix. Just a modified version of
+## "makeVector" by rdpeng
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrix_inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    matrix_inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) matrix_inverse <<- solve
+  getInverse <- function() matrix_inverse
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
